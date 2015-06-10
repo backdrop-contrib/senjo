22447916eb097a8d97158f01c987240c420934d8
docwilmot <docwilmot@gmail.com>
2015-06-10 12:43:10 -0500

    Add wrapper for content section

--- layout--senjo.tpl.php
+++ layout--senjo.tpl.php
@@ -34,42 +34,44 @@
     </section>
   <?php endif; ?>
 
-  <main class="main container l-container" role="main">
-    <div class="page-header">
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
+  <div class="l-main-wrapper">
+    <main class="main container l-container" role="main">
+      <div class="page-header">
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="title" id="page-title">
+            <?php print $title; ?>
+          </h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+      </div>
+
+      <?php if ($tabs): ?>
+        <div class="tabs">
+          <?php print $tabs; ?>
+        </div>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-    </div>
 
-    <?php if ($tabs): ?>
-      <div class="tabs">
-        <?php print $tabs; ?>
-      </div>
-    <?php endif; ?>
+      <?php print $action_links; ?>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 senjo-layouts-top panel-panel">
+            <?php print $content['top']; ?>
+          </div>
+        </div>
 
-    <?php print $action_links; ?>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12 senjo-layouts-top panel-panel">
-          <?php print $content['top']; ?>
+        <div class="row">
+          <div class="col-md-8 senjo-layouts-content panel-panel">
+            <?php print $content['content']; ?>
+          </div>
+          <div class="col-md-4 senjo-layouts-sidebar panel-panel">
+            <?php print $content['sidebar']; ?>
+          </div>
         </div>
       </div>
-
-      <div class="row">
-        <div class="col-md-8 senjo-layouts-content panel-panel">
-          <?php print $content['content']; ?>
-        </div>
-        <div class="col-md-4 senjo-layouts-sidebar panel-panel">
-          <?php print $content['sidebar']; ?>
-        </div>
-      </div>
-    </div>
-  </main>
+    </main>
+  </div>
 
   <?php if ($content['footer']): ?>
     <footer id="footer" class="l-footer" role="footer">
